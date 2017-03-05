84bafcaeb31cb255f3dad759e16e853380a73a8b
Aurélien DUMAINE <aurelien@dumaine.me>
2017-03-05 15:50:41 +0100

    Nginx cleaning

--- overwrite_before_init/etc/init_posbox_image.sh
+++ overwrite_before_init/etc/init_posbox_image.sh
@@ -13,26 +13,17 @@ export DEBIAN_FRONTEND=noninteractive
 
 mount /dev/sda1 /boot
 
+#echo "================>> Enable root user"
+#echo "================>> TODO after reboot : manually change root and pi paswords !"
 echo "root:rasp" | chpasswd
 
-#create dirs for ramdisks
-create_ramdisk_dir () {
-    mkdir "${1}_ram"
-}
 
-echo "================>> create ramdisk changes"
-create_ramdisk_dir "/var"
-create_ramdisk_dir "/etc"
-create_ramdisk_dir "/tmp"
-mkdir /root_bypass_ramdisks
-
-
-
-echo "================>> test0"
+#echo "================>> Update the package list"
 #apt-get update
+#echo "================>> Upgrade the system"
 #apt-get -y dist-upgrade
 
-PKGS_TO_INSTALL="w3m adduser postgresql-client python python-pycountry python-unidecode python-dateutil python-decorator python-docutils python-feedparser python-imaging python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-passlib python-psutil python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-pypdf python-reportlab python-requests python-tz python-vatnumber python-vobject python-werkzeug python-xlwt python-yaml postgresql python-gevent python-serial python-pip python-dev localepurge vim mc mg screen iw hostapd isc-dhcp-server git rsync console-data"
+PKGS_TO_INSTALL="w3m python-simplejson python-unittest2 adduser postgresql-client python python-pycountry python-unidecode python-dateutil python-decorator python-docutils python-feedparser python-imaging python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-passlib python-psutil python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-pypdf python-reportlab python-requests python-tz python-vatnumber python-vobject python-werkzeug python-xlwt python-yaml postgresql python-gevent python-serial python-pip python-dev localepurge vim mc mg screen iw hostapd isc-dhcp-server git rsync console-data"
 
 #PKGS_TO_INSTALL="adduser postgresql-client postgresql localepurge iw hostapd isc-dhcp-server console-data"
 
@@ -46,55 +37,81 @@ rm -rf /usr/share/doc
 # the latest pyusb from pip does not work either, usb.core.find() never returns
 # this may be fixed with libusb>2:1.0.11-1, but that's the most recent one in raspbian
 # so we install the latest pyusb that works with this libusb
-#pip install pyusb==1.0.0b1
-#pip install qrcode
-#pip install evdev
+pip install pyusb==1.0.0b1
+pip install qrcode
+pip install evdev
 
-echo "================>> test1"
+echo "================>> Add system user pi to usbusers system group"
 groupadd usbusers
 usermod -a -G usbusers pi
 usermod -a -G lp pi
 
 
-echo "================>> test2"
+echo "================>> Create postgres profile for pi system user"
 sudo -u postgres createuser -s pi
 mkdir /var/log/odoo
 chown pi:pi /var/log/odoo
 
-echo "================>> test3"
+echo "================>> Settle logrotate"
 # logrotate is very picky when it comes to file permissions
 chown -R root:root /etc/logrotate.d/
 chmod -R 644 /etc/logrotate.d/
 chown root:root /etc/logrotate.conf
 chmod 644 /etc/logrotate.conf
 
-echo "================>> test4"
+echo "================>> Add cron task to empty the /var/run/odoo/sessions directory"
 echo "* * * * * rm /var/run/odoo/sessions/*" | crontab -
 
 update-rc.d -f hostapd remove
 update-rc.d -f isc-dhcp-server remove
 
-echo "================>> systemctl changes"
+
+#create dirs for ramdisks
+create_ramdisk_dir () {
+    mkdir "${1}_ram"
+}
+
+echo "================>> Create ramdisk directories"
+create_ramdisk_dir "/var"
+create_ramdisk_dir "/etc"
+create_ramdisk_dir "/tmp"
+mkdir /root_bypass_ramdisks
+
+
+echo "================>> Enable ramdisk systemctl service"
 systemctl daemon-reload
 systemctl enable ramdisks.service
 systemctl disable dphys-swapfile.service
 
-echo "================>> setupcon changes"
+echo "================>> Enable setupcon"
 # https://www.raspberrypi.org/forums/viewtopic.php?p=79249
 # to not have "setting up console font and keymap" during boot take ages
 setupcon
 
 
-echo "================>> install python modules pycountry and unidecode"
-#pip install pycountry
-#pip install unidecode
-echo "================>> end"
+### This is actually done in the previous posbox_create_image.sh script in order to accelerate the image creation (do not do it each time we run the current script)
+#echo "================>> Add hw_cashlogy and hw_telium_payment_terminal modules"
+#if [ ! -d "/home/pi/odoo/addons/hw_cashlogy" ]; then
+#        wget https://github.com/shewolfParis/odoo-production/archive/9.0.zip
+#        unzip 9.0.zip
+#        cp -r odoo-production-9.0/extra_addons/hw_cashlogy /home/pi/odoo/addons
+#        cp -r odoo-production-9.0/extra_addons/hw_telium_payment_terminal /home/pi/odoo/addons
+#        rm -rf 9.0.zip
+#        rm -rf odoo-production-9.0
+#fi
+
+
+echo "================>> Install Apache2"
 apt-get -y install apache2
+
+echo "================>> Generate selfsigned certificate"
 mkdir /etc/apache2/ssl
 cd /etc/apache2/ssl
 
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=FR/ST=Denial/L=Paris/O=Dis/CN=posbox"  -keyout posbox.key  -out posbox.cert
 
+
+echo "================>> Enable Odoo vhost"
 a2enmod ssl
 a2enmod rewrite
 a2enmod proxy_http
@@ -104,4 +121,5 @@ a2ensite odoo
 a2dissite 000-default.conf
 service apache2 restart
 
+echo "================>> end"
 #reboot
