64ae5cac554676d748171130becad300cd0b0209
Aurélien DUMAINE <aurelien@dumaine.me>
2017-03-12 17:32:07 +0100

    Clean try_after_built

--- try_image_after_buid.sh
+++ try_image_after_buid.sh
@@ -21,28 +21,21 @@ sudo sed -i 's/,hw_escpos//g' ${MOUNT_POINT}/etc/rc.local
 sudo sed -i 's/^\(.\)/#\1/g' ${MOUNT_POINT}/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/led_status.sh
 
 sudo umount ${MOUNT_POINT}
+sleep 5
 rm -Rf ${MOUNT_POINT}
 
-# EXC the VM
+# EXEC the VM
 qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append 'root=/dev/sda2 panic=1 rootfstype=ext4 rw' -hda ${IMAGE_FILE} -net user,hostfwd=tcp::10022-:22,hostfwd=tcp::18069-:8069,hostfwd=tcp::10443-:443,hostfwd=tcp::10080-:80 -net nic
 
 mkdir ${MOUNT_POINT}
 sudo mount -v -o offset=$((512*${START_OF_ROOT_PARTITION})) -t ext4 ${IMAGE_FILE} ${MOUNT_POINT}
 
-
-exit(0)
-
-# changer fstab
+#CLEAN THE IMAGE FILE
 sed -i '' 's/^#//g' ${MOUNT_POINT}/etc/fstab
-
-# changer /etc/ld.so.preload
 sed -i '' 's/^#//g' ${MOUNT_POINT}/etc/ld.so.preload
-
-# supprimer hw_escpos des modules odoo à charger dans /etc/rc.local
 sed -i '' 's/,hw_scanner/,hw_scanner,hw_escpos/g' ${MOUNT_POINT}/etc/rc.local
-
-# changer /home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/led_status.sh
 sed -i '' 's/^#//g' ${MOUNT_POINT}/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/led_status.sh
 
 sudo umount ${MOUNT_POINT}
+sleep 5
 rm -Rf ${MOUNT_POINT}
