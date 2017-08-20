e9148288d03a2510c2c3b722fdb8e330e61c2f77
yuntux <yuntux@gmail.com>
2017-08-20 11:57:28 +0200

    Update update_posbox.sh

--- overwrite_before_init/home/pi/update_posbox.sh
+++ overwrite_before_init/home/pi/update_posbox.sh
@@ -4,16 +4,16 @@ CLONE_DIR="/home/pi/odoo"
 rm -rf "${CLONE_DIR}"
 
 mkdir -p  "${CLONE_DIR}"
-git clone -b 8.0 --no-checkout --depth 1 https://github.com/odoo/odoo.git "${CLONE_DIR}"
-cd "${CLONE_DIR}"
-git config core.sparsecheckout true
-echo "addons/web
-addons/web_kanban
-addons/hw_*
-addons/point_of_sale/tools/posbox/configuration
-openerp/
-odoo.py" | tee --append .git/info/sparse-checkout > /dev/null
-git read-tree -mu HEAD
+wget https://github.com/odoo/odoo/archive/9.0.zip
+unzip 9.0.zip
+cp -r odoo-9.0/addons/web ${CLONE_DIR}/addons
+cp -r odoo-9.0/addons/web_kanban ${CLONE_DIR}/addons
+cp -r odoo-9.0/addons/hw_* ${CLONE_DIR}/addons
+cp -r odoo-9.0/addons/point_of_sale/tools/posbox/configuration ${CLONE_DIR}/addons
+cp -r odoo-9.0/openerp/ ${CLONE_DIR}/
+cp -r odoo-9.0/odoo.py ${CLONE_DIR}/
+rm -rf 9.0.zip
+rm -rf odoo-9.0
 
 wget https://github.com/AwesomeFoodCoops/odoo-production/archive/9.0.zip
 unzip 9.0.zip
