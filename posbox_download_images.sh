ebbb623ee8ce854a3fcbf6dbddcbf9055ba12bb0
Iván Todorovich <ivan.todorovich@gmail.com>
2019-11-07 16:59:47 -0300

    [UPD] raspbian img

--- posbox_download_images.sh
+++ posbox_download_images.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "Downloading raspbian.img.."
-#wget 'https://downloads.raspberrypi.org/raspbian_lite_latest' -O raspbian.img.zip
-wget 'http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-07-05/2017-07-05-raspbian-jessie-lite.zip' -O raspbian.img.zip
+wget 'http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-11-15/2018-11-13-raspbian-stretch-lite.zip' -O raspbian.img.zip
 unzip raspbian.img.zip
 rm raspbian.img.zip
 
